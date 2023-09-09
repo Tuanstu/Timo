33fce27edb901f76cbb2feb30a875de023987076
DELL <DELL@DESKTOP-B91B0U5>
2023-09-09 23:51:07 +0700

    Add project files.

---
+++ Timo/Controllers/TInhLaiSuat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Timo.Models.TinhLaiSuat;
+
+namespace Timo.Controllers
+{
+    public class TInhLaiSuat : Controller
+    {
+        public IActionResult TinhLaiSuat()
+        {
+            TinhLaiSuat view = new TinhLaiSuat();
+            return View(view);
+        }
+        [HttpPost]
+        public IActionResult TinhLaiSuat(TinhLaiSuat input)
+        {
+            TinhLaiSuat model = new TinhLaiSuat(input.soTien, input.kyhan, input.laisuat);
+            //ViewData["soTien"] = soTien;
+            //ViewData["kyHan"] = kyhan;
+            //ViewData["laiSuat"] = laisuat;
+            //ViewData["SoTienLai"] = soTienLai;
+            //ViewData["TongSoTien"] = tongsotien;
+            return View(model);
+        }
+
+
+    }
+}
---
+++ Timo/Models/TinhLaiSuat/TinhLaiSuat.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Timo.Models.TinhLaiSuat
+{
+    public class TinhLaiSuat
+    {
+        public int soTien { get; set; }
+        public int kyhan { get; set; }
+        public double laisuat { get; set; }
+        public double soTienLai { get; set; }
+        public double tongsotien { get; set; }
+        public TinhLaiSuat() { }
+        public TinhLaiSuat(int soTien, int kyhan, double laisuat)
+        {
+            this.soTien = soTien;
+            this.kyhan = kyhan;
+            this.laisuat = laisuat;
+            soTienLai = soTien * laisuat * kyhan / 100;
+            tongsotien = soTien + soTienLai;
+        }
+    }
+}
